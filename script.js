a8a38fc4301d5566b8119ba6ce9a50203f1329bf
AyloSrd <aylo.srd@gmail.com>
2021-03-14 18:13:52 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+function generate(length = 12) {
+    var uppercase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    var lowercase = 'abcdefghijklmnopqrstuvwxyz';
+    var numbers = '0123456789';
+    var symbols = '!"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~';
+    var all = uppercase + lowercase + numbers + symbols;
+    var password = '';
+    for (var index = 0; index < length; index++) {
+        var character = Math.floor(Math.random() * all.length);
+        password += all.charAt(character);
+    }
+    return password;
+}
