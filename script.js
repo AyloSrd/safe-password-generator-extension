b6a25d9fed152bbcbcf83999ccb6a6191ed2fe82
AyloSrd <aylo.srd@gmail.com>
2021-03-16 00:59:13 +0100

    password generator class instantiatedand provvisory completed, including  Durstenfeld shuffle

--- script.js
+++ script.js
@@ -1,41 +1,34 @@
-// function generate(length = 12) {
-//     var uppercase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-//     var lowercase = 'abcdefghijklmnopqrstuvwxyz';
-//     var numbers = '0123456789';
-//     var symbols = '!"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~';
-//     var all = uppercase + lowercase + numbers + symbols;
-//     var password = '';
-//     for (var index = 0; index < length; index++) {
-//         var character = Math.floor(Math.random() * all.length);
-//         password += all.charAt(character);
-//     }
-//     return password;
-// }
-
 class PasswordGenerator {
-    constructor(params, length) {
-        this.params = params
-        this.length = length
+    constructor() {
         this.uppercase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
         this.lowercase = 'abcdefghijklmnopqrstuvwxyz';
         this.numbers = '0123456789';
         this.symbols = '!"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~';
-        this.all = this.uppercase + this.lowercase + this.numbers + this.symbols;
         this.password = ''
     }
 
-    generate = () => {
-        for (var index = 0; index < this.length; index++) {
-            var character = Math.floor(Math.random() * this.all.length);
-            this.password += this.all.charAt(character);
+    generate = (params, length) => {
+        this.password = params.reduce((psw, p) => psw + this[p].charAt(Math.floor(Math.random() * this[p].length)), '')
+        if (length) {
+            const all = params.reduce((psw, p) => psw + this[p], '')
+            for (let i = 0; i < length; i++) {
+                this.password += all.charAt(Math.floor(Math.random() * all.length))
+            }
         }
+        const provPswd = this.password.split('')
+        for (let i = provPswd.length - 1; i > 0; i--) {
+             const j = Math.floor(Math.random() * (i + 1))
+             const temp = provPswd[i]
+             provPswd[i] = provPswd[j]
+             provPswd[j] = temp
+        }
+        this.password = provPswd.join('')
         return this.password
     }
 }
 
 document.querySelector("#generation-tab > button").addEventListener('click', () => {
     console.log('clicked')
-    const Psw = new PasswordGenerator('yoo', 12)
-    console.log(Psw.generate())
-    document.getElementById('password').innerText = Psw.generate()
+    const Pswd = new PasswordGenerator()
+    document.getElementById('password').innerText = Pswd.generate(['uppercase', 'numbers', 'symbols', 'lowercase'], 50)
 })
