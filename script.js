7289122bd13e9a023c5ec3e155083f3b62e8d752
AyloSrd <aylo.srd@gmail.com>
2021-03-17 20:12:39 +0100

    added fonts and some css

--- script.js
+++ script.js
@@ -2,9 +2,9 @@ import PasswordGenerator from './PasswordGenerator.js'
 
 document.querySelector("#generation-tab > form").addEventListener('submit', e => {
     e.preventDefault()
-    const params = Array.from(document.querySelectorAll('#generation-tab > form > input[type=checkbox]:checked')).map(cbx => cbx.name)
+    const params = Array.from(document.querySelectorAll('#generation-tab > form > label> input[type=checkbox]:checked')).map(cbx => cbx.name)
     const pswdLength = Number(document.getElementById('pswd-length').value)
     console.log()
     const PswdGen = new PasswordGenerator()
-    document.getElementById('password').innerText = PswdGen.generate(params, pswdLength)
+    document.getElementById('password').value = PswdGen.generate(params, pswdLength)
 })
