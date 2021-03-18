a3c20d8e1996f952a9343cd26f0ce0d2f178f2ce
AyloSrd <aylo.srd@gmail.com>
2021-03-18 18:07:59 +0100

    se

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ import PasswordGenerator from './PasswordGenerator.js'
 
 document.querySelector("#generation-tab > form").addEventListener('submit', e => {
     e.preventDefault()
-    const params = Array.from(document.querySelectorAll('#generation-tab > form > label> input[type=checkbox]:checked')).map(cbx => cbx.name)
+    const params = Array.from(document.querySelectorAll('#generation-tab > form > div > label> input[type=checkbox]:checked')).map(cbx => cbx.name)
     const pswdLength = Number(document.getElementById('pswd-length').value)
     console.log()
     const PswdGen = new PasswordGenerator()
