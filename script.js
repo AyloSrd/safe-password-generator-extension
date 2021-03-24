1e524d2d7a5ea41c1613d6e6f25bf0deacc6a155
AyloSrd <aylo.srd@gmail.com>
2021-03-24 16:28:54 +0100

    started implementing save function

--- script.js
+++ script.js
@@ -1,10 +1,28 @@
 import PasswordGenerator from './PasswordGenerator.js'
+import Display from './Display.js'
 
-document.querySelector("#generation-tab > form").addEventListener('submit', e => {
+const secondaryForm =  document.querySelector("#generation-tab > form.small-form")
+const password = document.getElementById('password')
+const passwordCopy = document.getElementById('password-copy')
+
+document.querySelector("#generation-tab > .form").addEventListener('submit', e => {
     e.preventDefault()
     const params = Array.from(document.querySelectorAll('#generation-tab > form > div > label> input[type=checkbox]:checked')).map(cbx => cbx.name)
     const pswdLength = Number(document.getElementById('pswd-length').value)
     console.log()
     const PswdGen = new PasswordGenerator()
-    document.getElementById('password').value = PswdGen.generate(params, pswdLength)
+    password.value = PswdGen.generate(params, pswdLength)
 })
+
+secondaryForm.addEventListener('submit', e => {
+    e.preventDefault()
+    passwordCopy.value = password.value
+})
+
+secondaryForm.addEventListener('reset', e => {
+    e.preventDefault()
+    password.select()
+    document.execCommand('copy')
+})
+
+
