c8a01e6a1bde627a6002f3f213ea4639c5845b6a
AyloSrd <aylo.srd@gmail.com>
2021-03-25 19:33:15 +0100

    added populate storage function

--- script.js
+++ script.js
@@ -5,6 +5,37 @@ const secondaryForm =  document.querySelector("#generation-tab > form.small-form
 const saveForm = document.querySelector("#save-tab > form")
 const password = document.getElementById('password')
 const passwordCopy = document.getElementById('password-copy')
+const storageSection =  document.getElementById('storage-tab')
+
+const display = new Display()
+
+const createItem = (id, assName, str) => {
+    const itemContainer = document.createElement('div')
+    const form = document.createElement('form')
+    const assNameDisplay = document.createElement('label')
+    const strDisplay = document.createElement('input')
+    const copyButton = document.createElement('button')
+    const deleteButton = document.createElement('button')
+    copyButton.innerText= 'c'
+    deleteButton.innerText = 'x'
+    strDisplay.setAttribute('name', 'str')
+    assNameDisplay.setAttribute('for', 'str');
+    assNameDisplay.innerText = assName
+    strDisplay.value = display.showOnScreen(str)
+    form.appendChild(assNameDisplay)
+    form.appendChild(strDisplay)
+    itemContainer.appendChild(form)
+    itemContainer.appendChild(copyButton)
+    itemContainer.appendChild(deleteButton)
+    return itemContainer
+}
+
+const populateStorageSection = () => {
+    chrome.storage.sync.get(['sepg'], res => {
+        if (res.sepg.length === 0 || res.sepg === undefined)  storageSection.innerText = 'no items'
+        res.sepg.forEach(item => storageSection.appendChild(createItem(item.id, item.assName, item.str)))
+      })
+}
 
 document.querySelector("#generation-tab > .form").addEventListener('submit', e => {
     e.preventDefault()
@@ -14,6 +45,8 @@ document.querySelector("#generation-tab > .form").addEventListener('submit', e =
     const PswdGen = new PasswordGenerator()
     password.value = PswdGen.generate(params, pswdLength)
 })
+
+password.addEventListener('keydown', e => e.preventDefault())
 
 secondaryForm.addEventListener('submit', e => {
     e.preventDefault()
@@ -31,7 +64,7 @@ saveForm.addEventListener('submit', e => {
     const associatedName = document.getElementById('associated-name').value
     chrome.storage.sync.get(['sepg'], res => {
         const oldList = res.sepg ?? []
-        const newList = [...oldList, {id: `${Math.random()}`, assName: associatedName, str: passwordCopy.value}]
+        const newList = [...oldList, {id: `${Math.random()}`, assName: associatedName, str: display.putIn(passwordCopy.value)}]
         console.log(newList)
         chrome.storage.sync.set({sepg: newList}, () => {
             console.log('done')
@@ -39,4 +72,5 @@ saveForm.addEventListener('submit', e => {
       })
 })
 
+populateStorageSection()
 
